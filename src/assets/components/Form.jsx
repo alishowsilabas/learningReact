bff702847631e6677840fc5d2e06f70d46975b71
alishowsilabas <alissonsilabas@gmail.com>
2023-10-16 18:46:20 -0300

    multiplos inputs

--- src/assets/components/Form.jsx
+++ src/assets/components/Form.jsx
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 
 const Form = () => {
+  // const [nome, setNome] = useState("");
+  // const [sobrenome, setSobrenome] = useState("");
+  // const [idade, setIdade] = useState(0);
 
-  const [meuInputText, setMeuInputText]  = useState('')
-
-  const handleChange = (event) => {
-   setMeuInputText(event.target.value)
-   console.log(meuInputText);
-  }
+  const [estado, setEstado] = useState({
+    nome: '',
+    sobrenome: '',
+    idade: 0
+  })
 
   const handleClick = () => {
-    console.log(meuInputText);
+    console.log(estado.nome, estado.sobrenome, estado.idade);
+  };
+
+  const handleChange = (event) => {
+    setEstado({
+      ...estado,
+      [event.target.name]:event.target.value
+    })
   }
 
-  return  (
-    <form action="">
-      <input type="text" onChange={handleChange} value={meuInputText}/>
+  return (
+    <div>
+      <div>
+        <input type="text" onChange={handleChange} value={estado.nome} name="nome" />
+      </div>
+      <div>
+        <input type="text" onChange={handleChange} value={estado.sobrenome} name="sobrenome" />
+      </div>
+      <div>
+        <input type="text" onChange={handleChange} value={estado.idade} name="idade" />
+      </div>
+
       <button onClick={handleClick}>Clique</button>
-    </form>
-  )
+    </div>
+  );
 };
 
 export default Form;
