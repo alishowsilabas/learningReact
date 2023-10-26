f2e87825aaeb586ed6e6d10f7a8897d6bc8d0233
alishowsilabas <alissonsilabas@gmail.com>
2023-10-26 16:48:48 -0300

    exemplo de uso useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import Bar from "./assets/components/Bar";
+import Counter from "./assets/components/Counter";
 import Foo from "./assets/components/Foo";
 
 function App() {
   return (
     <div className="App">
-      <Foo />
-      <Bar />
+      <Counter />
     </div>
   );
 }
