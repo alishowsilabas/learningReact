c49e10d61c480fb254f891d98bfbc5f1ca6402a0
alishowsilabas <alissonsilabas@gmail.com>
2023-10-31 11:02:26 -0300

    testando Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import "./App.css";
-import Counter from "./assets/components/Counter";
+import React from "react";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    const retorno = await fetch("https://jsonplaceholder.typicode.com/users");
+    const dados = await retorno.json();
+    console.log(dados);
+  }
+
+  return <div>App</div>;
+};
 
 export default App;
