43d13739c99b953921a5b0f6e93a9c699010563e
alishowsilabas <alissonsilabas@gmail.com>
2023-10-31 16:54:59 -0300

    aprendendo utilizar controller.abort

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,31 @@ import axios from "axios";
 const App = () => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
 
-  const [contagem, setContagem] = useState(0)
+  const [contagem, setContagem] = useState(0);
 
   useEffect(() => {
+    const controller = new AbortController();
     setIsLoading(true);
     setTimeout(() => {
-      console.log("A requisição foi enviada")
-    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
-      setUsers(response.data);
-      console.log("A requisição retornou")
-      setIsLoading(false); 
-      }).catch(() => {
-        setIsLoading(false)
-        setError(true)
-      })
+      console.log("A requisição foi enviada");
+      setError(false);
+      axios.get("https://jsonplaceholder.typicode.com/users", {
+          signal: controller.signal
+        }).then(response => {
+          setUsers(response.data);
+          console.log("A requisição retornou");
+          setIsLoading(false);
+        }).catch(() => {
+          setIsLoading(false);
+          setError(true);
+        });
     }, 2000);
+
+    return () => {
+      controller.abort();
+    };
   }, [contagem]);
 
   return (
@@ -31,7 +39,9 @@ const App = () => {
         return <h3 key={item.id}>{item.name}</h3>;
       })}
       <h2>{error && <p>Algo inesperado aconteceu.</p>}</h2>
-      <button onClick={() => setContagem(contagem + 1)}>Contagem {contagem}</button>
+      <button onClick={() => setContagem(contagem + 1)}>
+        Contagem {contagem}
+      </button>
     </div>
   );
 };
