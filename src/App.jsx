6fe2239b7576e35fd0481ff6a2b7306bfaf71be6
alishowsilabas <alissonsilabas@gmail.com>
2023-11-17 16:25:32 -0300

    zerando arquivos locais para novos estudos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import React, { useEffect, useReducer } from "react";
-import Filho1 from "./assets/components/Filho1";
-import Filho2 from "./assets/components/Filho2";
-
+import React from 'react'
 
 const App = () => {
-
-
   return (
-    <div>
-     <Filho1 />
-     <Filho2 />
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
